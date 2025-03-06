77c09d20caaf55418d6bd2d68e0d6fb287bb1bc1
Mahmoud_AbdElazeem <abdelazeammahmoud@gmail.com>
2025-03-06 16:27:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'electron-update-app',
+  webDir: 'www'
+};
+
+export default config;
